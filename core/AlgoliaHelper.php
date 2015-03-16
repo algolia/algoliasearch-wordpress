04dfa085d4967789a10e092a5f3147a0d81b4c02
Maxime Locqueville <maxiloc@gmail.com>
2015-03-16 17:11:28 +0100

    fix empty global var

--- core/AlgoliaHelper.php
+++ core/AlgoliaHelper.php
@@ -56,6 +56,8 @@ class AlgoliaHelper
 
         $facets = array();
 
+        $customRankingTemp = array();
+
         $facets[] = "type";
 
         global $attributesToIndex;
@@ -63,7 +65,6 @@ class AlgoliaHelper
         global $attributesToHighlight;
         global $attributesToSnippet;
 
-        global $customRankingTemp;
 
         $defaultSettings = array(
             "attributesToIndex"     => $attributesToIndex,
