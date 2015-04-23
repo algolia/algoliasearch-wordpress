430397ddc8dee140dfa0957ad125f2da00c36cd0
Maxime Locqueville <maxiloc@gmail.com>
2015-04-23 14:39:50 +0200

    clean indexable_types view

--- admin/views/tabs/indexable_types.php
+++ admin/views/tabs/indexable_types.php
@@ -1,3 +1,31 @@
+<?php
+    $excluded_types = array('revision', 'nav_menu_item', 'acf', 'product_variation', 'shop_order', 'shop_order_refund', 'shop_coupon', 'shop_webhook', 'wooframework');
+    $types = array();
+    $i = 0;
+
+    foreach (get_post_types() as $type)
+    {
+        $count = wp_count_posts($type)->publish;
+
+        if ($count == 0 || in_array($type, $excluded_types))
+            continue;
+
+        $typeItem           = new stdClass();
+
+        $typeItem->name     = $type;
+        $typeItem->count    = $count;
+
+        $typeItem->order    = is_array($algolia_registry->indexable_types) && in_array($type, array_keys($algolia_registry->indexable_types))
+                                ? $algolia_registry->indexable_types[$type]['order'] : 10000 + $i;
+
+        $typeItem->checked  = is_array($algolia_registry->indexable_types) && in_array($type, array_keys($algolia_registry->indexable_types));
+
+        $typeItem->label    = isset($algolia_registry->indexable_types[$type]) ? $algolia_registry->indexable_types[$type]['name'] : "";
+
+        $types[]            = $typeItem;
+    }
+?>
+
 <div class="tab-content" id="_indexable-types">
     <form action="<?php echo site_url(); ?>/wp-admin/admin-post.php" method="post">
         <input type="hidden" name="action" value="update_indexable_types">
@@ -13,37 +41,24 @@
                         <?php endif; ?>
                     </tr>
 
-                    <?php $i = 0; ?>
-
-                    <?php foreach (get_post_types() as $type) : ?>
-                        <?php $count = wp_count_posts($type)->publish; ?>
-                        <?php if ($count == 0 || in_array($type, array('revision', 'nav_menu_item', 'acf', 'product_variation', 'shop_order', 'shop_order_refund', 'shop_coupon', 'shop_webhook', 'wooframework'))) { continue; } ?>
-                        <?php
-                        $order = -1;
-                        if (is_array($algolia_registry->indexable_types) && in_array($type, array_keys($algolia_registry->indexable_types)))
-                            $order = $algolia_registry->indexable_types[$type]['order'];
-                        ?>
-                        <?php if ($order != -1): ?>
-                            <tr data-order="<?php echo $order; ?>">
-                        <?php else: ?>
-                            <tr data-order="<?php echo (10000 + $i); $i++ ?>">
-                        <?php endif; ?>
-                        <td class="table-col-enabled">
-                            <input type="checkbox"
-                                   name="TYPES[<?php echo $type; ?>][SLUG]"
-                                   value="<?php echo $type; ?>"
-                                <?php checked(is_array($algolia_registry->indexable_types) && in_array($type, array_keys($algolia_registry->indexable_types))) ?>
-                                >
-                        </td>
-                        <td>
-                            <?php echo $type; ?> (<?php echo $count; ?>)
-                        </td>
-                        <?php if (in_array('autocomplete', $algolia_registry->type_of_search)): ?>
-                            <td style="white-space: nowrap;">
-                                <input type="text" value="<?php echo (isset($algolia_registry->indexable_types[$type]) ? $algolia_registry->indexable_types[$type]['name'] : "") ?>" name="TYPES[<?php echo $type; ?>][NAME]">
-                                <img width="10" src="<?php echo plugin_dir_url(__FILE__); ?>../../imgs/move.png">
+                    <?php foreach ($types as $typeItem) : ?>
+                        <tr data-order="<?php echo $typeItem->order; ?>">
+                            <td class="table-col-enabled">
+                                <input type="checkbox"
+                                       name="TYPES[<?php echo $typeItem->name; ?>][SLUG]"
+                                       value="<?php echo $typeItem->name; ?>"
+                                    <?php checked($typeItem->checked) ?>
+                                    >
                             </td>
-                        <?php endif; ?>
+                            <td>
+                                <?php echo $typeItem->name; ?> (<?php echo $typeItem->count; ?>)
+                            </td>
+                            <?php if (in_array('autocomplete', $algolia_registry->type_of_search)): ?>
+                            <td style="white-space: nowrap;">
+                                <input type="text" value="<?php echo $typeItem->label ?>" name="TYPES[<?php echo $typeItem->name; ?>][NAME]">
+                                <img width="10" src="<?php echo $move_icon_url; ?>">
+                            </td>
+                            <?php endif; ?>
                         </tr>
                     <?php endforeach; ?>
                 </table>
