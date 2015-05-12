275bc2e91f65fafef68a4b9b79d19b5c87a95412
Maxime Locqueville <maxiloc@gmail.com>
2015-05-12 13:43:30 +0200

    update to 0.0.6

--- algolia.php
+++ algolia.php
@@ -24,7 +24,7 @@ require_once(plugin_dir_path(__FILE__).'/core/QueryReplacer.php');
 require_once(plugin_dir_path(__FILE__).'/AlgoliaPlugin.php');
 require_once(plugin_dir_path(__FILE__).'/AlgoliaPluginAuto.php');
 
-\AlgoliaSearch\Version::$custom_value = " WORDPRESS (0.0.5)";
+\AlgoliaSearch\Version::$custom_value = " WORDPRESS (0.0.6)";
 
 
 new AlgoliaPlugin();
