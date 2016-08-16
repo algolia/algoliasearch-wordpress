8a10c231772cfc0b94d5991fbceef6750922a4d2
Raymond Rutjes <raymond.rutjes@gmail.com>
2016-08-16 17:09:05 +0200

    Update deps path

--- dev/update-deps.sh
+++ dev/update-deps.sh
@@ -3,7 +3,7 @@ set -e
 
 # Update PHP dependencies.
 # docker run --rm -v $(pwd):/app composer/composer update
-cd wordpress/wp-content/plugins/algolia/includes
+cd includes
 rm -rf libraries
 mkdir libraries
 cd libraries
@@ -12,12 +12,12 @@ git clone --depth 1 git@github.com:algolia/algoliasearch-client-php.git && rm -r
 git clone --depth 1 git@github.com:algolia/php-dom-parser.git && rm -rf php-dom-parser/.git
 git clone --depth 1 git@github.com:techcrunch/wp-async-task.git && rm -rf wp-async-task/.git
 
-cd ../../../../../..
+cd ../..
 
 # Update JS dependencies.
 docker run --rm -v $(pwd):/app node /bin/bash -c "cd /app; npm update"
-cp node_modules/algoliasearch/dist/algoliasearch.jquery.min.js wordpress/wp-content/plugins/algolia/public/js/
-cp node_modules/autocomplete.js/dist/autocomplete.jquery.min.js wordpress/wp-content/plugins/algolia/public/js/
-cp node_modules/instantsearch.js/dist/instantsearch.min.js wordpress/wp-content/plugins/algolia/public/js/
-cp node_modules/tether/dist/js/tether.min.js wordpress/wp-content/plugins/algolia/public/js/
+cp node_modules/algoliasearch/dist/algoliasearch.jquery.min.js public/js/
+cp node_modules/autocomplete.js/dist/autocomplete.jquery.min.js public/js/
+cp node_modules/instantsearch.js/dist/instantsearch.min.js public/js/
+cp node_modules/tether/dist/js/tether.min.js public/js/
 rm -rf node_modules
