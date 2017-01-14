b80557d0633768d7187bc5891ef4c5de8a774c14
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-01-14 12:54:32 +0100

    Apply YOAST SEO frontend optimizations when generating records for Algolia
    Resolves: #477

--- includes/class-algolia-compatibility.php
+++ includes/class-algolia-compatibility.php
@@ -4,6 +4,16 @@ class Algolia_Compatibility {
 
 	public function __construct() {
 		add_action( 'algolia_before_handle_task', array( $this, 'register_vc_shortcodes' ) );
+		add_action( 'algolia_before_handle_task', array( $this, 'enable_yoast_frontend' ) );
+	}
+
+	/**
+	 * @param Algolia_Task $task
+	 */
+	public function enable_yoast_frontend( Algolia_Task $task ) {
+		if ( class_exists( 'WPSEO_Frontend' ) && method_exists( 'WPSEO_Frontend', 'get_instance' ) ) {
+			WPSEO_Frontend::get_instance();
+		}
 	}
 
 	/**
