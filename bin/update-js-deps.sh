e5825aeb26ecb2c3c968e122edf77259bd6c5d3b
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-04-26 15:16:02 +0200

    feat(build): separate script for update PHP and JS deps

--- bin/update-js-deps.sh
+++ bin/update-js-deps.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Update PHP dependencies.
-# docker run --rm -v $(pwd):/app composer/composer update
-cd includes
-rm -rf libraries
-mkdir libraries
-cd libraries
-
-git clone --depth 1 git@github.com:algolia/algoliasearch-client-php.git && rm -rf algoliasearch-client-php/.git
-git clone --depth 1 git@github.com:algolia/php-dom-parser.git && rm -rf php-dom-parser/.git
-git clone --depth 1 git@github.com:techcrunch/wp-async-task.git && rm -rf wp-async-task/.git
-
-cd ../..
-
 # Update JS dependencies.
 yarn install && yarn upgrade
 
--- bin/update-php-deps.sh
+++ bin/update-php-deps.sh
@@ -13,17 +13,3 @@ git clone --depth 1 git@github.com:algolia/php-dom-parser.git && rm -rf php-dom-
 git clone --depth 1 git@github.com:techcrunch/wp-async-task.git && rm -rf wp-async-task/.git
 
 cd ../..
-
-# Update JS dependencies.
-yarn install && yarn upgrade
-
-cp node_modules/algoliasearch/dist/algoliasearch.jquery.js assets/js/algoliasearch/
-cp node_modules/algoliasearch/dist/algoliasearch.jquery.min.js assets/js/algoliasearch/
-
-cp node_modules/autocomplete.js/dist/autocomplete.js assets/js/autocomplete.js/
-cp node_modules/autocomplete.js/dist/autocomplete.min.js assets/js/autocomplete.js/
-
-cp node_modules/instantsearch.js/dist/instantsearch-preact.js assets/js/instantsearch.js/
-cp node_modules/instantsearch.js/dist/instantsearch-preact.min.js assets/js/instantsearch.js/
-
-rm -rf node_modules
