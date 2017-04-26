08762a2150998f1a7dbedba36dd2ffdba3397237
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-04-26 17:43:10 +0200

    refactor(php-dom): remove dependency of php-dom lib

--- bin/update-php-deps.sh
+++ bin/update-php-deps.sh
@@ -9,7 +9,6 @@ mkdir libraries
 cd libraries
 
 git clone --depth 1 git@github.com:algolia/algoliasearch-client-php.git && rm -rf algoliasearch-client-php/.git
-git clone --depth 1 git@github.com:algolia/php-dom-parser.git && rm -rf php-dom-parser/.git
 git clone --depth 1 git@github.com:techcrunch/wp-async-task.git && rm -rf wp-async-task/.git
 
 cd ../..
