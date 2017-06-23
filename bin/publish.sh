ded5b6863faafa38aa82aefdc44feeb1cd80ff76
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-06-23 15:28:51 +0200

    chore: fix publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,6 +2,12 @@
 
 set -eu
 
+readonly PACKAGE_VERSION=$(< package.json grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[:space:]')
+  
 git tag "v$PACKAGE_VERSION"
 git push --tags
 
