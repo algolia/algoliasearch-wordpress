9a9f2ac7d19a47eb7fbff0afa0e5d30c91a2d48f
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-06-23 14:48:28 +0200

    chore: remove unneeded bash scripts

--- bin/update-translations.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-find . -iname "*.php" > list
-xgettext --from-code=utf-8 --copyright-holder="Algolia" --package-name=algolia --language=PHP -f list --keyword=esc_html_e --keyword=esc_html__ --keyword=__ --keyword=_e -o languages/algolia.pot
-rm list
-
-
