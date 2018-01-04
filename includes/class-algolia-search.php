f3d1a38aaa390dcf57e9ce0b66c838350ebd604b
Raymond Rutjes <raymond.rutjes@gmail.com>
2018-01-04 11:01:41 +0100

    refactor(backend-search): simplify WP_Query hooking
    Closes: #693

--- includes/class-algolia-search.php
+++ includes/class-algolia-search.php
@@ -8,11 +8,6 @@ class Algolia_Search {
 	private $nb_hits;
 
 	/**
-	 * @var array
-	 */
-	private $ranked_post_ids;
-
-	/**
 	 * @var Algolia_Index
 	 */
 	private $index;
@@ -75,10 +70,8 @@ class Algolia_Search {
 			return;
 		}
 
-		add_filter( 'the_posts', array( $this, 'the_posts' ), 10, 2 );
 		add_filter( 'found_posts', array( $this, 'found_posts' ), 10, 2 );
 		add_filter( 'posts_search', array( $this, 'posts_search' ), 10, 2 );
-		add_filter( 'posts_clauses', array( $this, 'posts_clauses' ), 10, 2 );
 
 		// Store the total number of its, so that we can hook into the `found_posts`.
 		// This is useful for pagination.
@@ -89,7 +82,12 @@ class Algolia_Search {
 			$post_ids[] = $result['post_id'];
 		}
 
-		$this->ranked_post_ids = $post_ids;
+		// Make sure there are not results by tricking WordPress in trying to find
+		// a non existing post ID.
+		// Otherwise, the query returns all the results.
+		if ( empty( $post_ids ) ) {
+			$post_ids = array( 0 );
+		}
 
 		$query->set( 'posts_per_page', $posts_per_page );
 		$query->set( 'offset', 0 );
@@ -104,40 +102,10 @@ class Algolia_Search {
 
 		$query->set( 'post_type', $post_types );
 		$query->set( 'post__in', $post_ids );
+		$query->set( 'orderby', 'post__in' );
 
 		// Todo: this actually still excludes trash and auto-drafts.
 		$query->set( 'post_status', 'any' );
-	}
-
-	/**
-	 * This hook orders the posts according to Algolia's ranking instead
-	 * of the MySQL default ranking.
-	 *
-	 * @param array    $posts
-	 * @param WP_Query $query
-	 *
-	 * @return array
-	 */
-	public function the_posts( array $posts, WP_Query $query ) {
-		if ( ! $this->should_filter_query( $query ) ) {
-			return $posts;
-		}
-
-		$ranked_posts = array();
-
-		$ranked_post_ids = array_reverse( $this->ranked_post_ids );
-		do {
-			$ranked_post_id = array_pop( $ranked_post_ids );
-			foreach ( $posts as $post ) {
-				// Todo: This could be optimized.
-				if ( $ranked_post_id === $post->ID ) {
-					$ranked_posts[] = $post;
-					break;
-				}
-			}
-		} while ( ! empty( $ranked_post_ids ) );
-
-		return $ranked_posts;
 	}
 
 	/**
@@ -165,24 +133,5 @@ class Algolia_Search {
 	 */
 	public function posts_search( $search, WP_Query $query ) {
 		return $this->should_filter_query( $query ) ? '' : $search;
-	}
-
-	/**
-	 * Removes remaining unused SQL pieces.
-	 *
-	 * @param array    $pieces
-	 * @param WP_Query $query
-	 *
-	 * @return mixed
-	 */
-	public function posts_clauses( $pieces, WP_Query $query ) {
-		if ( ! $this->should_filter_query( $query ) ) {
-			return $pieces;
-		}
-
-		// We don't care about MySQL ordering. We will need to re-order with Algolia's ranking anyway.
-		$pieces['orderby'] = '';
-
-		return $pieces;
 	}
 }
