3b1184898e5d6d9a3791f2c4a8023ed6b71dc5d8
Raymond Rutjes <raymond.rutjes@gmail.com>
2018-01-04 11:00:25 +0100

    fix(admin): avoid leaving the page when buttons clicked on search page

--- includes/admin/partials/page-search.php
+++ includes/admin/partials/page-search.php
@@ -1,8 +1,8 @@
 <div class="wrap">
   <h1>
 	<?php echo esc_html( get_admin_page_title() ); ?>
-	<button class="algolia-reindex-button button button-primary" data-index="searchable_posts"><?php esc_html_e( 'Re-index search page records', 'algolia' ); ?></button>
-	<button class="algolia-push-settings-button button" data-index="searchable_posts"><?php esc_html_e( 'Push Settings', 'algolia' ); ?></button>
+	<button type="button" class="algolia-reindex-button button button-primary" data-index="searchable_posts"><?php esc_html_e( 'Re-index search page records', 'algolia' ); ?></button>
+	<button type="button" class="algolia-push-settings-button button" data-index="searchable_posts"><?php esc_html_e( 'Push Settings', 'algolia' ); ?></button>
   </h1>
   <form method="post" action="options.php">
 		<?php
