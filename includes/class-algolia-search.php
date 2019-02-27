2bdf12350715205aaafdd1be3913846d816785b5
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-02-27 13:58:38 +0100

    Moved to should filter function
    According to recommendation by product owner.

--- includes/class-algolia-search.php
+++ includes/class-algolia-search.php
@@ -29,7 +29,9 @@ class Algolia_Search {
 	 * @return bool
 	 */
 	private function should_filter_query( WP_Query $query ) {
-		return ! $query->is_admin && $query->is_search() && $query->is_main_query();
+		$should_filter = ! $query->is_admin && $query->is_search() && $query->is_main_query();
+		
+		return (bool) apply_filters( 'algolia_should_filter_query', $should_filter, $query );
 	}
 
 	/**
@@ -39,9 +41,7 @@ class Algolia_Search {
 	 */
 	public function pre_get_posts( WP_Query $query ) {
 
-		$should_filter_query = apply_filters('algolia_should_filter_query', $this->should_filter_query( $query ), $query);
-
-		if ( ! $should_filter_query ) {
+		if ( ! $this->should_filter_query( $query ) ) {
 			return;
 		}
 
